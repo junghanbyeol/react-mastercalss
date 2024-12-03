d91a8735baa4056ef207af45afabeb25bd90e68e
junghanbyeol <gksquf9720@naver.com>
2024-12-03 10:27:03 +0900

    with typescript

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+import Circle from "./Circle";
+
+function App() {
+  return (
+    <div>
+      <Circle bgColor={"red"} />
+      <Circle bgColor={"tomato"} />
+    </div>
+  );
+}
+
+export default App;
